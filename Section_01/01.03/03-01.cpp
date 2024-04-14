a62603df0d19ed55338b3ecc32b1904cf520fe4d
Luverduck <hyekko92@gmail.com>
2024-04-14 19:33:15 +0900

    01.03 변수

---
+++ Section_01/01.03/03-01.cpp
@@ -0,0 +1,23 @@
+// Section 01. C++의 기초적인 사용법
+// 01.03 변수
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// 변수의 선언
+	int var;
+
+	// 변수에 값을 할당
+	int x;
+	x = 10;
+
+	// 변수의 초기화
+	int y = 10;
+
+	return 0;
+}
