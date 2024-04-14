3cba77606cf92018d2be341b94268b66dff9ed11
Luverduck <hyekko92@gmail.com>
2024-04-14 17:04:30 +0900

    Add project files.

---
+++ Section_01/01.01/01_01.cpp
@@ -0,0 +1,17 @@
+// Section 01. C++의 기초적인 사용법
+// 01.01 프로그램의 구조
+
+// 전처리 명령
+#include <iostream>
+
+// 메인 함수
+int main(void)
+{
+	// 실행할 명령문
+	int x = 2;
+	x = 5;
+	int y = x + 3;
+	std::cout << x << std::endl;
+
+	return 0;
+};
