5f5f723843533f307bf2ad5e57e7c5a1a2a677bb
Luverduck <hyekko92@gmail.com>
2024-04-14 17:46:18 +0900

    01.02 주석

---
+++ Section_01/01.02/01_02.cpp
@@ -0,0 +1,27 @@
+// Section 01. C++의 기초적인 사용법
+// 01.02 주석
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// 한 줄 주석 (single-line comment)
+	// int var = 10;
+
+	// 여러 줄 주석 (multi-line comment)
+	/*
+		int i = 10;
+		double d = 1.1;
+	*/
+
+	// 여러 줄 주석을 중첩해서 사용할 경우 주석이 해제되어 컴파일 오류가 발생한다.
+	/*
+		/**/
+	*/
+
+	return 0;
+}
