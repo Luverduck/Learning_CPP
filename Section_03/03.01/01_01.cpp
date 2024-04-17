dafe8a55f11a57c422cd2d512066018d2930714f
Luverduck <hyekko92@gmail.com>
2024-04-18 02:18:05 +0900

    03.01 연산자와 표현식

---
+++ Section_03/03.01/01_01.cpp
@@ -0,0 +1,18 @@
+// Section 03. 연산자
+// 03.01 연산자와 표현식
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// 연산자(operator)와 피연산자(operand)
+	int i = 10;
+	// 연산자 : 대입 연산자 '='
+	// 피연산자 : 변수 i와 리터럴 상수 10
+
+	return 0;
+}
---
+++ Section_03/03.01/01_02.cpp
@@ -0,0 +1,19 @@
+// Section 03. 연산자
+// 03.01 연산자와 표현식
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// 표현식의 평가(evaluation)
+	1 + 2;
+	// 연산자 : +
+	// 피연산자 : 1, 2
+	// 표현식 평가를 통해 3이라는 값으로 환원된다.
+
+	return 0;
+}
