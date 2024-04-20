3a96f03f70100ee461aea6f306f624463eaf8a61
Luverduck <hyekko92@gmail.com>
2024-04-20 18:27:04 +0900

    04.01 흐름 제어의 개요

---
+++ Section_04/04.01/01_01.cpp
@@ -0,0 +1,19 @@
+// Section 04. 흐름 제어
+// 04.01 흐름 제어의 개요
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	std::cout << "I love you!" << std::endl;
+
+	exit(0);
+
+	std::cout << "Me too! " << std::endl;
+
+	return 0;
+}
