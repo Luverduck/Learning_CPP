970ae77caf06bb6421ce5a9239a6e392a24b369c
Luverduck <hyekko92@gmail.com>
2024-04-20 22:47:38 +0900

    05.04 변수의 연결 상태

---
+++ Section_05/05.04/04_01_il_func.cpp
@@ -0,0 +1,20 @@
+// Section 05. 변수와 메모리
+// 05.04 변수의 연결 상태
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+// 내부 연결된 전역 변수
+//extern int var_global_el;
+
+// 내부 연결된 전역 함수
+/*
+extern void externalLinkageTest()
+{
+	cout << "Call external function" << endl;
+	cout << var_global_il << endl;
+}
+*/
