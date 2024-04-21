4641d0ef5b4cc39bed6444891496b64c0f845c79
Luverduck <hyekko92@gmail.com>
2024-04-22 03:19:56 +0900

    06.05 메모리 동적 할당

---
+++ Section_06/06.05/05_01.cpp
@@ -0,0 +1,22 @@
+// Section 05. 포인터와 참조
+// 06.05 메모리 동적 할당
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// new 연산자
+	// 동적으로 메모리를 할당한 후 할당한 자료형의 포인터를 반환한다.
+	int* ptr_i = new int;
+	cout << sizeof(*ptr_i) << endl;
+
+	// delete 연산자
+	// 동적으로 할당한 메모리를 해제한다.
+	delete ptr_i;
+
+	return 0;
+};
---
+++ Section_06/06.05/05_02.cpp
@@ -0,0 +1,26 @@
+// Section 05. 포인터와 참조
+// 06.05 메모리 동적 할당
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <malloc.h> // _msize()
+
+using namespace std;
+
+int main()
+{
+	// 배열의 메모리 동적 할당
+	int* ptr_arr = new int[30];
+
+	// 배열에 동적 할당된 메모리의 크기
+	cout << _msize(ptr_arr) << endl;
+
+	// 배열의 길이
+	cout << _msize(ptr_arr) / sizeof(int) << endl;
+
+	// 배열의 동적 할당 메모리 해제
+	delete[] ptr_arr;
+
+	return 0;
+};
